0c9f96fe6f7460fd6fcd7dd0896e6016764394a6
mariokick <mariokick@windowslive.com>
2016-03-04 23:09:20 +0700

    update เพิ่มเติม method (เฉพาะโครงสร้าง)
    ยังไม่มีการทำงานภายใน

--- src/SystemNpruPool_Admin/Admin.java
+++ src/SystemNpruPool_Admin/Admin.java
@@ -10,5 +10,33 @@ package SystemNpruPool_Admin;
  * @author Godonlyknows
  */
 public class Admin {
-    
+    int admin_id;
+    String admin_username;
+    String admin_password;
+    String admin_firstname;
+    String admin_lastname;
+    String admin_cardid;
+    String admin_phonenumber;
+        public void ShowDetailAdmin(){
+    }
+    public void AddAdmin(int id ,String username,
+                                  String password,
+                                  String fname,
+                                  String lname,
+                                  String cardid,
+                                  String phonenumber
+                                  ){
+        
+    }
+    public void EditAdmin(int id ,String username,
+                                  String password,
+                                  String fname,
+                                  String lname,
+                                  String cardid,
+                                  String phonenumber){
+        
+    }
+    public void DeleteAdmin(String username){
+        
+    }
 }
